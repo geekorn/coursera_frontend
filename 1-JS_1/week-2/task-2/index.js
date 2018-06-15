f870cab7b0b70cda4d1fcee0dc3850889d95b869
Serj.Korneev <korneev.s@merlion.ru>
2018-06-15 11:48:31 +0300

    done task2 in week2

--- 1-JS_1/week-2/task-2/index.js
+++ 1-JS_1/week-2/task-2/index.js
@@ -3,5 +3,13 @@
  * @returns {String}
  */
 module.exports = function (hashtags) {
+  var uniqueHashtags = hashtags.reduce(function(acc, item) {
+    if (acc.indexOf(item.toLowerCase()) === -1) {
+      acc.push(item.toLowerCase());
+    }
 
+    return acc;
+  }, [])
+
+  return uniqueHashtags.join(', ');
 };
