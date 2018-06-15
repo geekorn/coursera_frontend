2c79413fec973a8e252be51aebe33b9f3e885ae7
Serj.Korneev <korneev.s@merlion.ru>
2018-06-15 10:49:52 +0300

    done task2 in week1

--- 1-JS_1/week-1/task-2/index.js
+++ 1-JS_1/week-1/task-2/index.js
@@ -4,5 +4,8 @@
  * @returns {Boolean}
  */
 module.exports = function (hours, minutes) {
+  hours = hours >= 0 && hours <= 23;
+  minutes = minutes >= 00 && minutes <= 59;
 
+  return hours && minutes;
 };
