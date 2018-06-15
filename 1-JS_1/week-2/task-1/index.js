2bb798d78d0095256855fd20099c52c155298a26
Serj.Korneev <korneev.s@merlion.ru>
2018-06-15 11:41:59 +0300

    done task1 in week2

--- 1-JS_1/week-2/task-1/index.js
+++ 1-JS_1/week-2/task-1/index.js
@@ -3,5 +3,13 @@
  * @returns {String[]}
  */
 module.exports = function (tweet) {
+  var hashTags = tweet.split(' ')
+    .filter(function(w) {
+      return w.indexOf('#') !== -1;
+    })
+    .map(function(h) {
+      return h.slice(1);
+    })
 
+  return hashTags;
 };
