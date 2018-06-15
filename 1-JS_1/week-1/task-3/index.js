96365f72868d4383714f82342d495b12c0d55be6
Serj.Korneev <korneev.s@merlion.ru>
2018-06-15 10:54:56 +0300

    done task3 in week1

--- 1-JS_1/week-1/task-3/index.js
+++ 1-JS_1/week-1/task-3/index.js
@@ -5,5 +5,22 @@
  * @returns {String}
  */
 module.exports = function (hours, minutes, interval) {
+  var newMinutes = minutes + interval;
+  var newHours = hours + Math.floor(newMinutes / 60);
+ 
+  if (newMinutes >= 60) {
+    newMinutes %= 60;  
+  }
+  if (newMinutes < 10) {
+    newMinutes = '0' + newMinutes;
+  }
 
+  if (newHours >= 24) {
+    newHours %= 24;
+  }
+  if (newHours < 10) {
+    newHours = '0' + newHours;
+  }
+
+  return newHours + ':' + newMinutes;
 };
