4ff506efba769ec3c590b51a3c4e9ce44cc3b273
Serj.Korneev <korneev.s@merlion.ru>
2018-06-15 12:55:04 +0300

    refactor code in task1 in week3

--- 1-JS_1/week-3/index.js
+++ 1-JS_1/week-3/index.js
@@ -1,77 +1,70 @@
+function prependZero(number) {
+  return number < 10 ? '0' + number : number
+}
+
+function formatDate(date) {    
+  var dateObject = {
+    year: prependZero(date.getFullYear()),
+    month: prependZero(date.getMonth() + 1),
+    day: prependZero(date.getDate()),
+    hours: prependZero(date.getHours()),
+    minutes: prependZero(date.getMinutes()),
+  }
+
+  return dateObject.year + '-' + dateObject.month + '-' + dateObject.day + ' ' + dateObject.hours + ':' + dateObject.minutes
+}
+
+function checkNumber(number) {
+  if (number < 0) {
+    throw TypeError('не может быть отрицательным')      
+  }
+}
+
+function changeDate(date, number, times) {
+  switch (times) {
+    case 'years':
+      date.setFullYear(date.getFullYear() + number);
+      break;
+    case 'months':
+      date.setMonth(date.getMonth() + number);
+      break;
+    case 'days':
+      date.setDate(date.getDate() + number);
+      break;
+    case 'hours':
+      date.setHours(date.getHours() + number);
+      break;
+    case 'minutes':
+      date.setMinutes(date.getMinutes() + number);
+      break;
+    default:
+      throw TypeError('нет такого периода времени')
+  }
+}
+
 /**
  * @param {String} date
  * @returns {Object}
  */
-// module.exports = 
-function time(date) {
+module.exports = function (date) {
   date = new Date(date);
-  this.value = formatDate(date);
-
-  function prependZero(number) {
-    return number < 10 ? '0' + number : number
-  }
-
-  function formatDate(date) {    
-    var dateObject = {
-      year: prependZero(date.getFullYear()),
-      month: prependZero(date.getMonth() + 1),
-      day: prependZero(date.getDate()),
-      hours: prependZero(date.getHours()),
-      minutes: prependZero(date.getMinutes()),
-    }
-
-    return dateObject.year + '-' + dateObject.month + '-' + dateObject.day + ' ' + dateObject.hours + ':' + dateObject.minutes
-  }
-
-  function changeDate(number, times, action) {
-    if (number < 0) {
-      throw TypeError('не может быть отрицательным')      
-    }
-
-    number = (action === 'diff') ? -number : number;
-
-    switch (times) {
-      case 'years':
-        date = date.setFullYear(date.getFullYear() + number);
-        break;
-      case 'months':
-        date = date.setMonth(date.getMonth() + number);
-        break;
-      case 'days':
-        date = date.setDate(date.getDate() + number);
-        break;
-      case 'hours':
-        date = date.setHours(date.getHours() + number);
-        break;
-      case 'minutes':
-        date = date.setMinutes(date.getMinutes() + number);
-        break;
-      default:
-        throw TypeError('нет такого периода времени')
-    }
-
-    date = new Date(date)
-
-    return formatDate(date);
-  }
-
-  this.add = function(number, times) {
-    this.value = changeDate(number, times, 'sum');
-
-    return this;
-  }
-
-  this.subtract = function(number, times) {
-    this.value = changeDate(number, times, 'diff');
-
-    return this;
-  }
 
   return {
-    value: this.value,
-    add: this.add,
-    subtract: this.subtract
+    get value() { return formatDate(date) },
+
+    add: function(number, times) {
+      checkNumber(number);
+      changeDate(date, number, times);
+  
+      return this;
+    },
+
+    subtract: function(number, times) {
+      checkNumber(number);
+      number *= -1;
+      changeDate(date, number, times);
+  
+      return this;
+    }
   }
 };
-
-module.exports = time
